84551a1694af2b5479b5d58d1a5b8d3ce004237d
anhducokok <pham.aduc11@gmail.com>
2026-04-22 23:40:51 +0700

    [anhducokok/4] setup library and enviroment

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService, Item } from './app.service';
 
 @Controller()
 export class AppController {
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('api/items')
+  getItems(): Item[] {
+    return this.appService.getItems();
   }
 }
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Item {
+  id: number;
+  name: string;
+  description: string;
+}
+
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getItems(): Item[] {
+    return [
+      { id: 1, name: 'Item A', description: 'Description for item A' },
+      { id: 2, name: 'Item B', description: 'Description for item B' },
+      { id: 3, name: 'Item C', description: 'Description for item C' },
+    ];
   }
 }

168d3ae1d53b994d51a599f244a1d6a0b780d5a0
anhducokok <pham.aduc11@gmail.com>
2026-04-23 12:06:49 +0700

    .

--- server/src/database/prisma.service.ts
+++ server/src/database/prisma.service.ts
@@ -1,17 +1,22 @@
-import {
+﻿import {
   Injectable,
   OnModuleInit,
   INestApplication,
   OnModuleDestroy,
 } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
-    super();
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    super({ adapter });
   }
 
   async onModuleInit() {

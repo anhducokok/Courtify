5f82e5a2c7222a70743e68df47b8747ae54e5d24
anhducokok <pham.aduc11@gmail.com>
2026-04-23 10:56:57 +0700

    setup prisma

---
+++ server/src/database/prisma.service.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  OnModuleInit,
+  INestApplication,
+  OnModuleDestroy,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  enableShutdownHooks(app: INestApplication) {
+    process.on('beforeExit', () => {
+      void app.close();
+    });
+  }
+}
